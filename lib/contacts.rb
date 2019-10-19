c8ff2e257f42565c7aa626f468dff775c09d9fcf
Alice Freda <alicefreda@gmail.com>
2019-10-19 17:25:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,14 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
+  contact.each do |person_hash|
+      if person_hash == "Freddy Mercury"
+            person_hash.each do |flavors|
+              flavors.delete(0)
+            end
+      end
+  end
 
 end
